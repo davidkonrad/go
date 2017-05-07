a2d0c09897eb1a1b4a6ba3a1eaabd4900226a041
davidkonrad <davidkonrad@gmail.com>
2017-05-07 16:46:14 +0200

    title and meta desc on produkt

--- app/scripts/controllers/produkt.js
+++ app/scripts/controllers/produkt.js
@@ -18,32 +18,36 @@ angular.module('gulveonlineApp')
 			Lookup.setPassData({ produkt_navn: $scope.produkt.navn });
 
 			Lookup.formatProdukt($scope.produkt);
-			Meta.setTitle($scope.produkt.navn);
 
-			var meta = $scope.produkt.navn + '.';
-			if ($scope.produkt.kategori) {
-				meta += $scope.produkt.kategori + '. ';
-			}
-			if ($scope.produkt.sort) {
-				meta += 'Træsort '+ $scope.produkt.sort.toLowerCase() + '. ';
-			}
-			if ($scope.produkt.dimension) {
-				meta += 'Dim. '+ $scope.produkt.dimension + '. ';
-			}
-			if ($scope.produkt.kvalitet) {
-				meta += $scope.produkt.dimension + ' sortering. ';
-			}
-			if ($scope.produkt.profil) {
-				meta += $scope.produkt.profil + ' profil. ';
-			}
-			if ($scope.produkt.overflade) {
-				meta += $scope.produkt.overflade + '. ';
-			}
-			if ($scope.produkt.pris_enhed && $scope.produkt.enhed) {
-				meta += 'DKK '+ $scope.produkt.pris_enhed + ' /' + $scope.produkt.enhed + '. ';
-			}
+			Meta.setTitle($scope.produkt.meta_title || $scope.produkt.navn);
 
-			Meta.setDescription(meta);
+			if ($scope.produkt.meta_desc) {
+				Meta.setDescription($scope.produkt.meta_desc);
+			} else {
+				var meta = $scope.produkt.navn + '.';
+				if ($scope.produkt.kategori) {
+					meta += $scope.produkt.kategori + '. ';
+				}
+				if ($scope.produkt.sort) {
+					meta += 'Træsort '+ $scope.produkt.sort.toLowerCase() + '. ';
+				}
+				if ($scope.produkt.dimension) {
+					meta += 'Dim. '+ $scope.produkt.dimension + '. ';
+				}
+				if ($scope.produkt.kvalitet) {
+					meta += $scope.produkt.dimension + ' sortering. ';
+				}
+				if ($scope.produkt.profil) {
+					meta += $scope.produkt.profil + ' profil. ';
+				}
+				if ($scope.produkt.overflade) {
+					meta += $scope.produkt.overflade + '. ';
+				}
+				if ($scope.produkt.pris_enhed && $scope.produkt.enhed) {
+					meta += 'DKK '+ $scope.produkt.pris_enhed + ' /' + $scope.produkt.enhed + '. ';
+				}
+				Meta.setDescription(meta);
+			}
 
 		});
 

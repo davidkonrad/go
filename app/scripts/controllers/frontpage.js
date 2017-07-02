f961e72dd6721473adf59fb0b4d9614645c4bb92
davidkonrad <davidkonrad@gmail.com>
2017-07-02 10:16:56 +0200

    fixed race condition on frontpage

--- app/scripts/controllers/frontpage.js
+++ app/scripts/controllers/frontpage.js
@@ -13,30 +13,37 @@ angular.module('gulveonlineApp')
 		Meta.setTitle('gulve.online - import og produktion af kvalitets trægulve');
 		Meta.setDescription('Førende importør af massive plankegulve, parketstave, sildeben og mosaik. Vores leverandører er baseret på FSC-certificeret skovdrift.');
 
-		function initProdukter() {
-			$scope.produkter.forEach(function(produkt) {
-				Lookup.formatProdukt(produkt);				
-			});
-		}
-				
-		ESPBA.get('produkter', { aktiv: 1, forside: 1 }, { limit: 12, orderBy : { field: 'edited_timestamp', order: 'desc' }}).then(function(r) {
-			$scope.produkter = r.data;
-			var newLimit = 12-$scope.produkter.length;
-			if (newLimit>0) {
-				ESPBA.get('produkter', { aktiv: 1, forside: 0 }, { limit: newLimit, orderBy : 'rand()' }).then(function(r) {
-					$scope.produkter = $scope.produkter.concat(r.data);
-					initProdukter();
-				});
-			} else {
-				initProdukter();
-			}
-		});
+		function init() {
 
-		$scope.getBackgroundStyle = function(produkt) {
-			return {		
-				'background-image':'url(' + produkt.billede + ')'
+			function initProdukter() {
+				$scope.produkter.forEach(function(produkt) {
+					Lookup.formatProdukt(produkt);				
+				});
+			}
+				
+			ESPBA.get('produkter', { aktiv: 1, forside: 1 }, { limit: 12, orderBy : { field: 'edited_timestamp', order: 'desc' }}).then(function(r) {
+				$scope.produkter = r.data;
+				var newLimit = 12-$scope.produkter.length;
+				if (newLimit>0) {
+					ESPBA.get('produkter', { aktiv: 1, forside: 0 }, { limit: newLimit, orderBy : 'rand()' }).then(function(r) {
+						$scope.produkter = $scope.produkter.concat(r.data);
+						initProdukter();
+					});
+				} else {
+					initProdukter();
+				}
+			});
+
+			$scope.getBackgroundStyle = function(produkt) {
+				return {		
+					'background-image':'url(' + produkt.billede + ')'
+				};
 			};
-		};
+		}
+
+		Lookup.init().then(function() {
+			init();
+		});
 
 
 }]);
